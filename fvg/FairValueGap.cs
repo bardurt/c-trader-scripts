7e873a7d6fb6005d913631528523e387c1877c58
Bardur Thomsen <brur88@gmail.com>
2022-08-14 12:22:30 -0500

    Update FairValueGap.cs
    fix typose in variable name

--- fvg/FairValueGap.cs
+++ fvg/FairValueGap.cs
@@ -23,68 +23,66 @@ namespace cAlgo
         public int minGap { get; set; }
 
         // Size of the pip 
-        [Parameter("Pip Size", DefaultValue = 0.0001, MinValue = 0.0001, MaxValue = 1)]
+        [Parameter("Pip Size", DefaultValue = 5E-05, MinValue = 1E-05, MaxValue = 1)]
         public double pipsize { get; set; }
 
         // Draw horizontal lines to highlight the Fair Value Gap
         [Parameter("Paint Horizontal Lines", DefaultValue = false)]
         public bool paintHorizontalLines { get; set; }
 
-        // Lemgth of the lines for highlighting the Fair Value Gap
-        [Parameter("Horizontal Lines Length", DefaultValue = 10, MinValue = 5, MaxValue = 50)]
-        public int horizontalLinesLength { get; set; }
 
         public override void Calculate(int index)
         {
-            var nextBar = index;
-            var studyIndex = index - 1;
-            var previousBar = index + 2;
+            var nextBar = index - 1;
+            var studyIndex = index - 2;
+            var previousBar = index - 3;
 
-            var prevoiousCandleHigh = MarketSeries.High.Last(2);
-            var prevoiousCCandleLow = MarketSeries.Low.Last(2);
+            var prevCandleHigh = MarketSeries.High.Last(2);
+            var prevCandleLow = MarketSeries.Low.Last(2);
 
             var nextCandleHigh = MarketSeries.High.Last(0);
             var nextCandleLow = MarketSeries.Low.Last(0);
 
 
             // bearish fair value gap
-            if (prevoiousCCandleLow > nextCandleHigh)
+            if (prevCandleLow > nextCandleHigh)
             {
-                if (isGap(prevoiousCCandleLow, nextCandleHigh))
+                if (isFvg(prevCandleLow, nextCandleHigh))
                 {
-                    drawFvg(studyIndex, prevoiousCCandleLow, nextCandleHigh);
+                    drawFvg(nextBar, prevCandleLow, nextCandleHigh);
                 }
             }
 
             // bullish fair value gap
-            if (nextCandleLow > prevoiousCandleHigh)
+            if (nextCandleLow > prevCandleHigh)
             {
-                if (isGap(nextCandleLow, prevoiousCandleHigh))
+                if (isFvg(nextCandleLow, prevCandleHigh))
                 {
-                    drawFvg(studyIndex, nextCandleLow, prevoiousCandleHigh);
+                    drawFvg(nextBar, nextCandleLow, prevCandleHigh);
                 }
             }
 
         }
 
-        private bool isGap(double high, double low)
+        private bool isFvg(double high, double low)
         {
             double gapInPips = minGap * pipsize;
 
-            if ((high - low) > gapInPips)
+            if ((high - low) >= gapInPips)
             {
                 return true;
             }
 
             return false;
         }
-
+        
+   
         private void drawFvg(int index, double high, double low)
         {
             if (paintHorizontalLines)
             {
-                ChartObjects.DrawLine("FVG TOP" + index, index, high, index + horizontalLinesLength, high, color, 1, LineStyle.Solid);
-                ChartObjects.DrawLine("FVG BOTTOM" + index, index, low, index + horizontalLinesLength, low, color, 1, LineStyle.Solid);
+                ChartObjects.DrawLine("FVG TOP" + index, index, high, index, high, color, 1, LineStyle.Solid);
+                ChartObjects.DrawLine("FVG BOTTOM" + index, index, low, index, low, color, 1, LineStyle.Solid);
             }
             ChartObjects.DrawLine("FVG" + index, index, high, index, low, color, indicatorWidth, LineStyle.Solid);
         }
