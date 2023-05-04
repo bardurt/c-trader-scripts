d2b88fd1c671e682510e6f491da22502eb9a3bff
Bardur Thomsen <brur88@gmail.com>
2023-05-03 21:45:21 -0500

    Update FairValueGap.cs
    clean up code

--- fvg/FairValueGap.cs
+++ fvg/FairValueGap.cs
@@ -6,62 +6,148 @@ using cAlgo.API.Indicators;
 namespace cAlgo
 {
 
-    [Indicator(IsOverlay = true, AccessRights = AccessRights.None)]
+    [Indicator(IsOverlay = true, AccessRights = AccessRights.FullAccess)]
     public class FairValueGap : Indicator
     {
-
+    
         // Set the color for this indicator
-        [Parameter("Indicator Color", DefaultValue = Colors.Yellow)]
-        public Colors color { get; set; }
-
+        [Parameter("FVG Color", DefaultValue = Colors.Blue)]
+        public Colors FvgColor { get; set; }
+        
         // Width of this indicator
         [Parameter("Indicator Width", DefaultValue = 3, MinValue = 1, MaxValue = 5)]
-        public int indicatorWidth { get; set; }
+        public int IndicatorWidth { get; set; }
 
         // The size of a Fair Value Gap
-        [Parameter("Min Gap (Pips)", DefaultValue = 1, MinValue = 1, MaxValue = 5)]
-        public int minGap { get; set; }
+        [Parameter("Min Gap (Pips)", DefaultValue = 1, MinValue = 0, MaxValue = 5)]
+        public int MinGap { get; set; }
 
         // Size of the pip 
-        [Parameter("Pip Size", DefaultValue = 5E-05, MinValue = 1E-05, MaxValue = 1)]
-        public double pipsize { get; set; }
+        [Parameter("Pips per point", DefaultValue = 1, MinValue = 0.1, MaxValue = 10000)]
+        public double PipSize { get; set; }
+        
+        [Parameter("Lookback Days", DefaultValue = 5, MinValue = 2, MaxValue = 60)]
+        public int LookbackDays { get; set; }
+       
+        
+        private DateTime studyDate;
+        private bool Prepared = false;
+        private double PipScalar = 1;
+        
+        protected override void Initialize()
+        {
+            studyDate = DateTime.Now.AddDays(-(LookbackDays+1));
+            Prepared = false;
+        }
+
 
         public override void Calculate(int index)
         {
+        
+            if(!Prepared){
+                double high = MarketSeries.High[index];
+                double low = MarketSeries.Low[index];
+                double diff = high - low;
+                PipScalar = diff < 0.01 ? 10000 : 1;
+                Prepared = true;
+            }
+           
+            DateTime current = MarketSeries.OpenTime[index];
+
+            if (current < studyDate)
+            {
+                return;
+            }
+            
+            
             var nextBar = index - 1;
-            var studyIndex = index - 2;
-            var previousBar = index - 3;
-
-            var prevCandleHigh = MarketSeries.High.Last(2);
-            var prevCandleLow = MarketSeries.Low.Last(2);
-
-            var nextCandleHigh = MarketSeries.High.Last(0);
-            var nextCandleLow = MarketSeries.Low.Last(0);
-
-
-            // bearish fair value gap
-            if (prevCandleLow > nextCandleHigh)
-            {
-                if (isFvg(prevCandleLow, nextCandleHigh))
-                {
-                    drawFvg(nextBar, prevCandleLow, nextCandleHigh);
-                }
+            
+            if(index < 2){
+                return;
             }
 
-            // bullish fair value gap
-            if (nextCandleLow > prevCandleHigh)
+            var prevCandleHigh = MarketSeries.High[index-2];
+            var prevCandleLow = MarketSeries.Low[index-2];
+            
+            var studyCandleOpen = MarketSeries.Open[index-1];
+            var studyCandleClose = MarketSeries.Close[index-1];
+
+            var nextCandleHigh = MarketSeries.High[index];
+            var nextCandleLow = MarketSeries.Low[index];
+            
+        
+            if (IsFvg(prevCandleLow, nextCandleHigh))
             {
-                if (isFvg(nextCandleLow, prevCandleHigh))
+                  
+                double top = prevCandleLow;
+                double bottom = nextCandleHigh;
+                        
+                if(IsBullish(studyCandleOpen, studyCandleClose))
                 {
-                    drawFvg(nextBar, nextCandleLow, prevCandleHigh);
+                    if(prevCandleLow > studyCandleClose)
+                    {
+                        top = studyCandleClose;
+                    }
+                            
+                    if(nextCandleHigh > studyCandleClose)
+                    {
+                        bottom = studyCandleClose;
+                    }
+                } 
+                else 
+                {
+                    if(prevCandleLow > studyCandleOpen)
+                    {
+                        top = studyCandleOpen;
+                    }
+                           
+                    if(nextCandleHigh < studyCandleClose)
+                    {
+                        bottom = studyCandleClose;
+                    }
                 }
+                DrawFvg("FVG_BEARISH_BAR_"+index, nextBar, top, bottom, FvgColor);
             }
 
+
+      
+            if (IsFvg(nextCandleLow, prevCandleHigh))
+            {
+                double top = nextCandleLow;
+                double bottom = prevCandleHigh;
+                       
+                if(IsBullish(studyCandleOpen, studyCandleClose))
+                {
+                    if(nextCandleLow > studyCandleClose)
+                    {
+                        top = studyCandleClose;
+                    }
+                            
+                    if(prevCandleHigh < studyCandleOpen)
+                    {
+                        bottom = studyCandleOpen;
+                    }
+                } 
+                else 
+                {
+                    if(prevCandleLow > studyCandleOpen)
+                    {
+                        top = studyCandleOpen;
+                    }
+                        
+                    if(nextCandleHigh < studyCandleClose)
+                    {
+                        bottom = studyCandleClose;
+                    }
+                }
+                DrawFvg("FVG_BULLISH_BAR_"+index, nextBar, top, bottom, FvgColor);
+            }
         }
 
-        private bool isFvg(double high, double low)
+        
+        private bool IsFvg(double high, double low)
         {
-            double gapInPips = minGap * pipsize;
+            double gapInPips = MinGap / PipScalar;
 
             if ((high - low) >= gapInPips)
             {
@@ -72,9 +158,20 @@ namespace cAlgo
         }
         
    
-        private void drawFvg(int index, double high, double low)
+        private void DrawFvg(String label, int index, double high, double low, Colors color)
         {
-            ChartObjects.DrawLine("FVG" + index, index, high, index, low, color, indicatorWidth, LineStyle.Solid);
+            ChartObjects.DrawLine(label, index, high, index, low, color, IndicatorWidth, LineStyle.Solid);
+        }
+        
+        
+        private bool IsBullish(double open, double close){
+        
+            if(close > open)
+            {
+                return true;
+            }
+           
+            return false;
         }
     }
 }
